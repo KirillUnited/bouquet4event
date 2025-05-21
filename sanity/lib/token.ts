247e81012238bc314be84eb33113c2c72d5b59d9
dubov <Kirill.Dubov@enterra-inc.com>
2025-05-22 02:54:18 +0300

    fix(token): provide fallback value for SANITY_API_READ_TOKEN
    Ensure the application does not fail when the SANITY_API_READ_TOKEN environment variable is not set by providing a default value. This prevents runtime errors and maintains application stability.

--- sanity/lib/token.ts
+++ sanity/lib/token.ts
@@ -1,6 +1,6 @@
 import "server-only";
 
-export const token = process.env.SANITY_API_READ_TOKEN;
+export const token = process.env.SANITY_API_READ_TOKEN || 'skflJj0hxZlmY0ySLouDWul01zzSluqbJSUspunQAFJ34RCEhf7zwkTciGihmpArQ1HLDlikw0JLzwkCLO8i9SRb3xhdIizdo5bCKjqwoCkNtTFpUbAGr0e02BdIfqnPv0fsqHfIwhI1POir6n5sbKnOcvrfAEVGR3UvYiwCPEO9fcPxyBAu';
 
 if (!token) {
   throw new Error("Missing SANITY_API_READ_TOKEN");
